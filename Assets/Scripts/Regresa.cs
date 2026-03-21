876f7502b0ace0a3c1d17568cd7c29f22d557c1d
Regiina Hernandez <A01802688@tec.mx>
2026-03-20 19:25:34 -0600

    UI Menu

---
+++ Assets/Scripts/Regresa.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Regresa : MonoBehaviour
+{
+    private UIDocument menu;
+    private Button botonRegresa;
+
+    void OnEnable()
+    {
+        menu = GetComponent<UIDocument>();
+        var root = menu.rootVisualElement; //no sabes el tipo de dato que es rootVisualElement, por eso se usa var
+
+        botonRegresa = root.Q<Button>("botonRegresa");
+        botonRegresa.clicked += Regresar;
+
+    }
+
+    void OnDisable()
+    {
+        botonRegresa.clicked -= Regresar;
+    }
+
+    void Regresar()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
