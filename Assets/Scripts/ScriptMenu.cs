82a0e659c0e0a94f9549d5133579386c76601dd0
Regiina Hernandez <A01802688@tec.mx>
2026-03-21 09:54:36 -0600

    Menu Scripts
    Botón Jugar funcional

--- Assets/Scripts/ScriptMenu.cs
+++ Assets/Scripts/ScriptMenu.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
-public class ScriptMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private UIDocument menu;
+    private Button botonJugar;
+    private Button botonAyuda;
+    private Button botonCreditos;
+    private Button botonCerrar;
+
+    void OnEnable()
     {
-        
+        menu = GetComponent<UIDocument>();
+        var root = menu.rootVisualElement;
+
+        // Buscamos el botón
+        botonJugar = root.Q<Button>("botonJugar");
+
+        // Solo registramos si lo encontró
+        if (botonJugar != null) 
+        {
+            botonJugar.RegisterCallback<ClickEvent>(AbrirMapita);
+        }
+        else 
+        {
+            Debug.LogError("ERROR: No se encontró un botón con el nombre 'botonJugar' en el UI Builder.");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        botonJugar.UnregisterCallback<ClickEvent>(AbrirMapita);
+        //botonRegresa
     }
+
+    private void AbrirMapita(ClickEvent evt)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   /* private void AbrirAyuda(ClickEvent evn)
+    {
+        SceneManager.LoadScene(2);
+    }*/
+
 }
+
